e8e0ef6515ee0285f0b56ebee5b76cd9199021d7
NerdsNest <akshayasenthilvel2004@gmail.com>
2025-12-02 21:56:55 +0530

    added

---
+++ java-practice/PalindromeNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) 
+            return false;
+        int rev = 0;
+        while (x > rev) 
+        {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return x == rev || x == rev / 10;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int x=sc.nextInt();
+        System.out.println(isPalindrome(x));
+        sc.close();
+    }
+}
+    
+
