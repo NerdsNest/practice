2bf39c96c54da6b07ff3019e466171e50db4bb0c
NerdsNest <akshayasenthilvel2004@gmail.com>
2026-01-16 21:53:59 +0530

    added

---
+++ java-practice/ConvertIntoCm.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ConvertIntoCm {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the inch: ");
+        int inch=sc.nextInt();
+        System.out.println("The converted answer is:"+ inch*2.54);
+
+    }
+    
+}
