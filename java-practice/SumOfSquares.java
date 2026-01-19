bc47b47b2d0aa1adafc207e1d22cbfb9653ee6ae
NerdsNest <akshayasenthilvel2004@gmail.com>
2026-01-19 20:58:12 +0530

    added

---
+++ java-practice/SumOfSquares.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumOfSquares {
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of terms");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=Math.pow(i,2);
+        }
+        System.out.println(sum);
+    }
+}
